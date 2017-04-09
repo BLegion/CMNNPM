c4d1dee4497788764f43ec4d5b1873f4f21268fa
Huy Truong <thebingel@gmail.com>
2017-04-09 22:40:52 +0700

    Dungdan

---
+++ CMNNPM/BM1/BM1_dungdan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMNNPM.BM1
+{
+    public partial class BM1_dungdan : Form
+    {
+        public BM1_dungdan()
+        {
+            InitializeComponent();
+        }
+    }
+}
